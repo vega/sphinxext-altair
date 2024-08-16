ec1a9824d883a1c65390897eec2a117235318ec6
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-08-16 16:27:23 +0100

    refactor: Fix lints, typing, minor perf tweaks

--- sphinxext_altair/altairplot.py
+++ sphinxext_altair/altairplot.py
@@ -1,6 +1,5 @@
 """
-Altair Plot Sphinx Extension
-============================
+Altair Plot Sphinx Extension.
 
 This extension provides a means of inserting live-rendered Altair plots within
 sphinx documentation using the directive ``altair-plot``. You can also use it
@@ -48,42 +47,49 @@ again overrides it. It should look something like this::
 
     # conf.py
     # ...
-    altairplot_links = {'editor': True, 'source': True, 'export': True}
+    altairplot_links = {"editor": True, "source": True, "export": True}
     # ...
 
 If this configuration is not specified, all are set to True.
 """
 
+from __future__ import annotations
+
 import contextlib
 import io
 import json
 import os
-import sys
+import typing as t
 import warnings
-from typing import Any, Callable, Dict, List, Optional, Union
+from functools import lru_cache
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Callable, ClassVar, Literal
 
-import altair as alt
 import jinja2
-import sphinx.application
-import sphinx.environment
-from altair.utils.execeval import eval_block
 from docutils import nodes
 from docutils.parsers.rst import Directive
 from docutils.parsers.rst.directives import flag, unchanged
 from sphinx.locale import _
 
-if sys.version_info >= (3, 8):
-    from typing import Literal
-else:
-    from typing_extensions import Literal
+import altair as alt
+from altair.utils.execeval import eval_block
+from altair.utils.schemapi import SchemaValidationError
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+    from sphinx.environment import BuildEnvironment as _BuildEnvironment
+
+    class BuildEnvironment(_BuildEnvironment):
+        _altair_namespaces: dict[str, Any]
+
 
 # These default URLs can be changed in conf.py; see setup() below.
-VEGA_JS_URL_DEFAULT = "https://cdn.jsdelivr.net/npm/vega@{}".format(alt.VEGA_VERSION)
-VEGALITE_JS_URL_DEFAULT = "https://cdn.jsdelivr.net/npm/vega-lite@{}".format(
-    alt.VEGALITE_VERSION
+VEGA_JS_URL_DEFAULT = f"https://cdn.jsdelivr.net/npm/vega@{alt.VEGA_VERSION}"
+VEGALITE_JS_URL_DEFAULT = (
+    f"https://cdn.jsdelivr.net/npm/vega-lite@{alt.VEGALITE_VERSION}"
 )
-VEGAEMBED_JS_URL_DEFAULT = "https://cdn.jsdelivr.net/npm/vega-embed@{}".format(
-    alt.VEGAEMBED_VERSION
+VEGAEMBED_JS_URL_DEFAULT = (
+    f"https://cdn.jsdelivr.net/npm/vega-embed@{alt.VEGAEMBED_VERSION}"
 )
 
 
@@ -109,48 +115,48 @@ VGL_TEMPLATE = jinja2.Template(
 
 
 class altair_plot(nodes.General, nodes.Element):
-    pass
+    body: list[str]
 
 
-def purge_altair_namespaces(
-    app: sphinx.application.Sphinx,
-    env: sphinx.environment.BuildEnvironment,
-    docname: str,
-) -> None:
+def purge_altair_namespaces(app: Sphinx, env: BuildEnvironment, docname: str) -> None:
     if not hasattr(env, "_altair_namespaces"):
         return
     env._altair_namespaces.pop(docname, {})
 
 
-DEFAULT_ALTAIRPLOT_LINKS = {"editor": True, "source": True, "export": True}
+DEFAULT_ALTAIRPLOT_LINKS: dict[str, bool] = {"editor": True, "source": True, "export": True}  # fmt: off
 
 
-def validate_links(links: str) -> Union[Dict[str, bool], Literal[False]]:
-    if links.strip().lower() == "none":
+@lru_cache(maxsize=256)
+def strip_lower(content: str, /) -> str:
+    return content.strip().lower()
+
+
+def validate_links(links: str) -> dict[str, bool] | Literal[False]:
+    if strip_lower(links) == "none":
         return False
 
     links_split = links.strip().split()
-    diff = set(links_split) - set(DEFAULT_ALTAIRPLOT_LINKS.keys())
-    if diff:
-        raise ValueError("Following links are invalid: {}".format(list(diff)))
-    return {link: link in links_split for link in DEFAULT_ALTAIRPLOT_LINKS}
+    defaults = DEFAULT_ALTAIRPLOT_LINKS
+    if set(links_split) <= defaults.keys():
+        return {s: s in links_split for s in defaults}
+    else:
+        diff = set(links_split) - defaults.keys()
+        msg = f"Following links are invalid: {list(diff)}"
+        raise ValueError(msg)
 
 
 def validate_output(output: str) -> str:
-    output = output.strip().lower()
-    if output not in ["plot", "repr", "stdout", "none"]:
-        raise ValueError(":output: flag must be one of [plot|repr|stdout|none]")
+    output = strip_lower(output)
+    if output not in {"plot", "repr", "stdout", "none"}:
+        msg = ":output: flag must be one of [plot|repr|stdout|none]"
+        raise ValueError(msg)
     return output
 
 
-def validate_div_class(output: str) -> str:
-    return output.strip().lower()
-
-
 class AltairPlotDirective(Directive):
-    has_content = True
-
-    option_spec: Dict[str, Union[flag, unchanged, Callable[[str], Any]]] = {
+    has_content: ClassVar[bool] = True
+    option_spec: ClassVar[dict[str, Callable[[str], Any]]] = {
         "hide-code": flag,
         "remove-code": flag,
         "code-below": flag,
@@ -160,90 +166,75 @@ class AltairPlotDirective(Directive):
         "links": validate_links,
         "chart-var-name": unchanged,
         "strict": flag,
-        "div_class": validate_div_class,
+        "div_class": strip_lower,
     }
 
-    def run(self) -> List[nodes.Element]:
-        env: sphinx.environment.BuildEnvironment = self.state.document.settings.env
+    def run(self) -> list[nodes.Element]:
+        env = t.cast("BuildEnvironment", self.state.document.settings.env)
         app = env.app
 
         hide_code = "hide-code" in self.options
-        remove_code = "remove-code" in self.options
         code_below = "code-below" in self.options
-        strict = "strict" in self.options
-        div_class: Optional[str] = self.options.get("div_class", None)
 
         if not hasattr(env, "_altair_namespaces"):
-            env._altair_namespaces = {}  # type: ignore[attr-defined]
+            env._altair_namespaces = {}
         namespace_id = self.options.get("namespace", "default")
-        namespace = env._altair_namespaces.setdefault(  # type: ignore[attr-defined]
-            env.docname, {}
-        ).setdefault(namespace_id, {})
-
-        code = "\n".join(self.content)
+        namespace = env._altair_namespaces.setdefault(env.docname, {}).setdefault(
+            namespace_id, {}
+        )
 
         # Show code
+        code = "\n".join(self.content)
         source_literal = nodes.literal_block(code, code)
         source_literal["language"] = "python"
 
         # get the name of the source file we are currently processing
         rst_source = self.state_machine.document["source"]
-        rst_dir = os.path.dirname(rst_source)
-        rst_filename = os.path.basename(rst_source)
+        rst_fp = Path(rst_source)
 
         # use the source file name to construct a friendly target_id
         serialno = env.new_serialno("altair-plot")
-        rst_base = rst_filename.replace(".", "-")
-        div_id = "{}-altair-plot-{}".format(rst_base, serialno)
-        target_id = "{}-altair-source-{}".format(rst_base, serialno)
+        rst_base = rst_fp.name.replace(".", "-")
+        div_id = f"{rst_base}-altair-plot-{serialno}"
+        target_id = f"{rst_base}-altair-source-{serialno}"
         target_node = nodes.target("", "", ids=[target_id])
 
         # create the node in which the plot will appear;
         # this will be processed by html_visit_altair_plot
-        plot_node = altair_plot()
-        plot_node["target_id"] = target_id
-        plot_node["div_id"] = div_id
-        plot_node["div_class"] = div_class
-        plot_node["code"] = code
-        plot_node["namespace"] = namespace
-        plot_node["relpath"] = os.path.relpath(rst_dir, env.srcdir)
-        plot_node["rst_source"] = rst_source
-        plot_node["rst_lineno"] = self.lineno
-        plot_node["links"] = self.options.get(
-            "links", app.builder.config.altairplot_links
+        plot_node = altair_plot(
+            target_id=target_id,
+            div_id=div_id,
+            div_class=self.options.get("div_class", None),
+            code=code,
+            namespace=namespace,
+            relpath=os.path.relpath(rst_fp.parent, env.srcdir),
+            rst_source=rst_source,
+            rst_lineno=self.lineno,
+            links=self.options.get("links", app.builder.config.altairplot_links),
+            output=self.options.get("output", "plot"),
+            strict="strict" in self.options,
+            **{"chart-var-name": self.options.get("chart-var-name", None)},
         )
-        plot_node["output"] = self.options.get("output", "plot")
-        plot_node["chart-var-name"] = self.options.get("chart-var-name", None)
-        plot_node["strict"] = strict
-
         if "alt" in self.options:
             plot_node["alt"] = self.options["alt"]
 
-        result = [target_node]
-
+        result: list[nodes.Element] = [target_node]
         if code_below:
-            result += [plot_node]
-
+            result.append(plot_node)
         if hide_code:
             html = "<details><summary><a>Click to show code</a></summary>"
-            raw_html = nodes.raw("", html, format="html")
-            result += [raw_html]
-
-        if not remove_code:
-            result += [source_literal]
-
+            result.append(nodes.raw("", html, format="html"))
+        if "remove-code" not in self.options:
+            result.append(source_literal)
         if hide_code:
             html = "</details>"
-            raw_html = nodes.raw("", html, format="html")
-            result += [raw_html]
-
+            result.append(nodes.raw("", html, format="html"))
         if not code_below:
-            result += [plot_node]
-
+            result.append(plot_node)
         return result
 
 
-def html_visit_altair_plot(self, node: nodes.Element) -> None:
+def html_visit_altair_plot(self: altair_plot, node: nodes.Element) -> None:  # noqa: C901
     # Execute the code, saving output and namespace
     namespace = node["namespace"]
     try:
@@ -252,22 +243,22 @@ def html_visit_altair_plot(self, node: nodes.Element) -> None:
             chart = eval_block(node["code"], namespace)
         stdout = f.getvalue()
     except Exception as err:
-        message = "altair-plot: {}:{} Code Execution failed:" "{}: {}".format(
-            node["rst_source"], node["rst_lineno"], err.__class__.__name__, str(err)
+        msg = (
+            f"altair-plot: {node['rst_source']}:{node['rst_lineno']} "
+            f"Code Execution failed: {type(err).__name__}: {err!s}"
         )
         if node["strict"]:
-            raise ValueError(message) from err
+            raise ValueError(msg) from err
         else:
-            warnings.warn(message, stacklevel=1)
+            warnings.warn(msg, stacklevel=1)
             raise nodes.SkipNode from err
 
-    chart_name = node["chart-var-name"]
-    if chart_name is not None:
-        if chart_name not in namespace:
-            raise ValueError(
-                "chart-var-name='{}' not present in namespace" "".format(chart_name)
-            )
-        chart = namespace[chart_name]
+    if chart_name := node.get("chart-var-name", None):
+        if chart_name in namespace:
+            chart = namespace[chart_name]
+        else:
+            msg = f"chart-var-name='{chart_name}' not present in namespace"
+            raise ValueError(msg)
 
     output = node["output"]
 
@@ -279,23 +270,23 @@ def html_visit_altair_plot(self, node: nodes.Element) -> None:
         else:
             output_literal = nodes.literal_block(stdout, stdout)
             output_literal["language"] = "none"
-            node.extend([output_literal])
+            node.append(output_literal)
     elif output == "repr":
         if chart is None:
             raise nodes.SkipNode
         else:
-            rep = "    " + repr(chart).replace("\n", "\n    ")
+            rep = f"    {chart!r}".replace("\n", "\n    ")
             repr_literal = nodes.literal_block(rep, rep)
             repr_literal["language"] = "none"
-            node.extend([repr_literal])
+            node.append(repr_literal)
     elif output == "plot":
         if isinstance(chart, alt.TopLevelMixin):
             # Last line should be a chart; convert to spec dict
             try:
                 spec = chart.to_dict()
-            except alt.utils.schemapi.SchemaValidationError as err:
-                raise ValueError("Invalid chart: {0}".format(node["code"])) from err
-            actions = node["links"]
+            except SchemaValidationError as err:
+                msg = f"Invalid chart: {node['code']}"
+                raise ValueError(msg) from err
 
             # Pass relevant info into the template and append to the output
             html = VGL_TEMPLATE.render(
@@ -304,54 +295,43 @@ def html_visit_altair_plot(self, node: nodes.Element) -> None:
                 spec=json.dumps(spec),
                 mode="vega-lite",
                 renderer="canvas",
-                actions=json.dumps(actions),
+                actions=json.dumps(node["links"]),
             )
             self.body.append(html)
         else:
-            warnings.warn(
-                "altair-plot: {}:{} Malformed block. Last line of "
-                "code block should define a valid altair Chart object."
-                "".format(node["rst_source"], node["rst_lineno"]),
-                stacklevel=1,
+            msg = (
+                f"altair-plot: {node['rst_source']}:{node['rst_lineno']} Malformed block. "
+                "Last line of code block should define a valid altair Chart object."
             )
+            warnings.warn(msg, stacklevel=1)
         raise nodes.SkipNode
 
 
-def generic_visit_altair_plot(self, node: nodes.Element) -> None:
-    if "alt" in node.attributes:
-        self.body.append(_("[ graph: %s ]") % node["alt"])
-    else:
-        self.body.append(_("[ graph ]"))
+def generic_visit_altair_plot(self: altair_plot, node: nodes.Element) -> None:
+    self.body.append(
+        _("[ graph: %s ]") % node["alt"] if "alt" in node.attributes else _("[ graph ]")
+    )
     raise nodes.SkipNode
 
 
-def depart_altair_plot(self, node: nodes.Element) -> None:
-    return
+def depart_altair_plot(self: altair_plot, node: nodes.Element) -> None: ...
 
 
-def builder_inited(app: sphinx.application.Sphinx) -> None:
+def builder_inited(app: Sphinx) -> None:
     app.add_js_file(app.config.altairplot_vega_js_url)
     app.add_js_file(app.config.altairplot_vegalite_js_url)
     app.add_js_file(app.config.altairplot_vegaembed_js_url)
 
 
-def setup(app: sphinx.application.Sphinx) -> Dict[str, str]:
-    setup.app = app  # type: ignore[attr-defined]
-    setup.config = app.config  # type: ignore[attr-defined]
-    setup.confdir = app.confdir  # type: ignore[attr-defined]
-
+def setup(app: Sphinx) -> dict[str, str]:
     app.add_config_value("altairplot_links", DEFAULT_ALTAIRPLOT_LINKS, "env")
-
     app.add_config_value("altairplot_vega_js_url", VEGA_JS_URL_DEFAULT, "html")
     app.add_config_value("altairplot_vegalite_js_url", VEGALITE_JS_URL_DEFAULT, "html")
     app.add_config_value(
         "altairplot_vegaembed_js_url", VEGAEMBED_JS_URL_DEFAULT, "html"
     )
-
     app.add_directive("altair-plot", AltairPlotDirective)
-
     app.add_css_file("altair-plot.css")
-
     app.add_node(
         altair_plot,
         html=(html_visit_altair_plot, depart_altair_plot),
@@ -360,8 +340,6 @@ def setup(app: sphinx.application.Sphinx) -> Dict[str, str]:
         text=(generic_visit_altair_plot, depart_altair_plot),
         man=(generic_visit_altair_plot, depart_altair_plot),
     )
-
     app.connect("env-purge-doc", purge_altair_namespaces)
     app.connect("builder-inited", builder_inited)
-
     return {"version": "0.1"}
