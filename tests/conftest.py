cd29ce6cbcd17a03f42a91cfff5688f65d485281
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-08-16 16:11:33 +0100

    fix: Add compat for removed `sphinx.testing.path.path`
    Was deprecated in `(3, 9)` but removed in later versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
+import sys
+
 import pytest
-from sphinx.testing.path import path
 
 pytest_plugins = "sphinx.testing.fixtures"
 
@@ -7,7 +8,15 @@ pytest_plugins = "sphinx.testing.fixtures"
 # Exclude 'roots' dirs for pytest test collector
 collect_ignore = ["roots"]
 
+if sys.version_info >= (3, 9):
+    from pathlib import Path
 
-@pytest.fixture(scope="session")
-def rootdir():
-    return path(__file__).parent.abspath() / "roots"
+    @pytest.fixture(scope="session")
+    def rootdir():
+        return Path(__file__).parent / "roots"
+else:
+    from sphinx.testing.path import path
+
+    @pytest.fixture(scope="session")
+    def rootdir():
+        return path(__file__).parent.abspath() / "roots"
